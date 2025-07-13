0cceeb01b4b2ab6ba06f2a8fdbe90bb4aa21b0bd
virage81 <karimovminds@gmail.com>
2025-07-13 23:21:08 +0400

    feat: integrate translation logic

--- src/features/translation-widget/ui/TranslationWidget.tsx
+++ src/features/translation-widget/ui/TranslationWidget.tsx
@@ -1,66 +1,81 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
-import {
-  Paper,
-  Typography,
-  IconButton,
-  Box,
-  TextField,
-} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
-import { useDrag } from "../lib/useDrag";
-import { TranslationLanguageSelector } from "../../translation-language-selector/ui";
+import { useDebounce } from '@/shared/hooks/useDebounce';
+import { translate } from '@/shared/lib/translation';
+import CloseIcon from '@mui/icons-material/Close';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import { Box, IconButton, Paper, TextField, Typography } from '@mui/material';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { TranslationLanguageSelector } from '../../translation-language-selector/ui';
+import { useDrag } from '../lib/useDrag';
 
 export interface TranslationWidgetProps {
-  onClose: () => void;
+	onClose: () => void;
 }
 
-export const TranslationWidget: React.FC<TranslationWidgetProps> = ({
-  onClose,
-}) => {
-  const widgetRef = React.useRef<HTMLDivElement>(null);
+export const TranslationWidget: React.FC<TranslationWidgetProps> = ({ onClose }) => {
+	const { i18n } = useTranslation();
+
+	const widgetRef = React.useRef<HTMLDivElement>(null);
   const [sourceText, setSourceText] = useState("");
   const [translatedText, setTranslatedText] = useState("");
 
-  const { position, setPosition, handleMouseDown } = useDrag();
+	const { position, setPosition, handleMouseDown } = useDrag();
 
-  useLayoutEffect(() => {
-    if (widgetRef.current) {
-      const width = widgetRef.current.offsetWidth;
-      const height = widgetRef.current.offsetHeight;
-      setPosition({
-        x: window.innerWidth / 2 - width / 2,
-        y: window.innerHeight / 2 - height / 2,
-      });
-    }
-  }, []);
+	const translateText = async () => {
+		if (!sourceText.length) return;
 
-  useEffect(() => {
-    const handleMouseMove = () => {
-      if (!widgetRef.current) return;
+		try {
+			const { translations } = await translate({
+				sourceLanguageCode: i18n.language,
+				targetLanguageCode: 'en',
+				texts: [sourceText],
+			});
+			const translatedText = translations.map(item => item.text).join(' ');
+			setTranslatedText(translatedText);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-      const rect = widgetRef.current.getBoundingClientRect();
-      const threshold = 1;
+	useDebounce(translateText, 500, [sourceText]);
 
-      const nearBoundary =
-        rect.left < threshold ||
-        rect.top < threshold ||
-        window.innerWidth - rect.right < threshold ||
-        window.innerHeight - rect.bottom < threshold;
+	useLayoutEffect(() => {
+		if (widgetRef.current) {
+			const width = widgetRef.current.offsetWidth;
+			const height = widgetRef.current.offsetHeight;
+			setPosition({
+				x: window.innerWidth / 2 - width / 2,
+				y: window.innerHeight / 2 - height / 2,
+			});
+		}
+	}, []);
 
-      if (nearBoundary) {
-        onClose();
-      }
-    };
+	useEffect(() => {
+		const handleMouseMove = () => {
+			if (!widgetRef.current) return;
+
+			const rect = widgetRef.current.getBoundingClientRect();
+			const threshold = 1;
+
+			const nearBoundary =
+				rect.left < threshold ||
+				rect.top < threshold ||
+				window.innerWidth - rect.right < threshold ||
+				window.innerHeight - rect.bottom < threshold;
+
+			if (nearBoundary) {
+				onClose();
+			}
+		};
 
     window.addEventListener("mousemove", handleMouseMove);
     return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, [onClose]);
+	}, [onClose]);
 
-  return (
-    <Paper
-      ref={widgetRef}
-      sx={{
+	return (
+		<Paper
+			ref={widgetRef}
+			sx={{
         position: "fixed",
         top: position.y,
         left: position.x,
@@ -83,40 +98,40 @@ export const TranslationWidget: React.FC<TranslationWidgetProps> = ({
         <Box display="flex" alignItems="center">
           <DragIndicatorIcon sx={{ mr: 1, color: "action.active" }} />
           <Typography variant="h6" fontWeight="bold">
-            Переводчик
-          </Typography>
-        </Box>
-        <IconButton onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      </Box>
+						Переводчик
+					</Typography>
+				</Box>
+				<IconButton onClick={onClose}>
+					<CloseIcon />
+				</IconButton>
+			</Box>
 
-      <Box mt={3}>
-        <TranslationLanguageSelector />
-      </Box>
+			<Box mt={3}>
+				<TranslationLanguageSelector />
+			</Box>
 
-      <Box mt={3}>
-        <TextField
+			<Box mt={3}>
+				<TextField
           label="Исходный текст"
-          multiline
-          rows={3}
-          fullWidth
-          value={sourceText}
+					multiline
+					rows={3}
+					fullWidth
+					value={sourceText}
           onChange={(e) => setSourceText(e.target.value)}
-          autoFocus
-        />
-      </Box>
+					autoFocus
+				/>
+			</Box>
 
-      <Box mt={3}>
-        <TextField
+			<Box mt={3}>
+				<TextField
           label="Перевод"
-          multiline
-          rows={3}
-          fullWidth
-          value={translatedText}
+					multiline
+					rows={3}
+					fullWidth
+					value={translatedText}
           onChange={(e) => setTranslatedText(e.target.value)}
-        />
-      </Box>
-    </Paper>
-  );
+				/>
+			</Box>
+		</Paper>
+	);
 };
